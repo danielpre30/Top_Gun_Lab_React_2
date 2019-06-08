45d97ed112bed2f42dea6ac8dbbb2f6c3eeab996
Juan David Martinez <juan.restrepo@digitalamericas.ai>
2019-06-08 12:35:24 -0500

    EventHandlers

--- src/App.js
+++ src/App.js
@@ -1,42 +1,90 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import ColorCard from "./components/ColorCard";
 import { COLORS } from "./mocked-data/colors";
 
 class App extends Component {
   state = {
-    colors: COLORS
-  }
+    colors: COLORS,
+    filterText: "",
+    nameText: "",
+    lightClassText: "",
+    darkClassText: "",
+    isLight: false
+  };
 
-  changeTone = (id) => {
-    this.setState((prevState) => {
+  changeTone = id => {
+    this.setState(prevState => {
       const oldColors = prevState.colors;
-      return ({
+      return {
         colors: oldColors.map(color => {
           const isSelectedColor = color.id === id;
-          return (
-            isSelectedColor
+          return isSelectedColor
             ? { ...color, isLight: !color.isLight }
-            : color
-          );
+            : color;
         })
-      })
-    })
-  }
+      };
+    });
+  };
 
   deleteColor = (e, id) => {
     e.stopPropagation();
-    this.setState((prevState) => {
+    this.setState(prevState => {
       const oldColors = prevState.colors;
-      return ({
+      return {
         colors: oldColors.filter(color => color.id !== id)
-      })
-    })
+      };
+    });
+  };
+
+  handleFilter(e, field) {
+    const value = e.target.value;
+    this.setState({
+      [field]: value  //Los corchetes son para hacer referencia a la clave a partir de un string
+    });
+  }
+
+  handleIsLight(e) {
+    const checked = e.target.checked;
+    this.setState({
+      isLight: checked
+    });
+  }
+
+  createColor(e) {
+    e.preventDefault();
+    this.setState(prevState => {
+      const oldColors = prevState.colors;
+      return {
+        colors: [
+          ...oldColors,
+          {
+            id: Math.random(),
+            name: prevState.nameText,
+            isLight: prevState.isLight,
+            darkClass: prevState.darkClassText,
+            lightClass: prevState.lightClassText
+          }
+        ],
+        nameText: "",
+        lightClassText: "",
+        darkClassText: "",
+        isLight: false
+      };
+    });
   }
 
   render() {
-    const { colors } = this.state;
-
+    const {
+      colors,
+      filterText,
+      nameText,
+      lightClassText,
+      darkClassText
+    } = this.state;
+    const filteredColors = colors.filter(color => {
+      return color.name.includes(filterText);
+    });
     return (
       <div className="App">
         <h1 className="color-cards__title">COLOR CARDS</h1>
@@ -45,39 +93,64 @@ class App extends Component {
             placeholder="Filter by color name"
             className="filter-field"
             type="text"
+            onChange={e => {
+              this.handleFilter(e, "filterText");
+            }}
           />
         </div>
         <main className="color-cards-container">
-          {colors.map(color => (
+          {filteredColors.map(color => (
             <ColorCard
               key={color.id}
               changeColor={() => this.changeTone(color.id)}
-              deleteColor={(e) => this.deleteColor(e, color.id)}
+              deleteColor={e => this.deleteColor(e, color.id)}
               {...color}
             />
           ))}
         </main>
         <h2 className="create-color-title">Create a color!</h2>
-        <form className="create-color-form" onSubmit={this.createColor}>
+        <form
+          className="create-color-form"
+          onSubmit={e => {
+            this.createColor(e);
+          }}
+        >
           <input
             type="text"
             placeholder="name"
+            onChange={e => {
+              this.handleFilter(e, "nameText");
+            }}
+            value={nameText}
           />
           <input
             type="text"
             placeholder="light class"
+            onChange={e => {
+              this.handleFilter(e, "lightClassText");
+            }}
+            value={lightClassText}
           />
           <input
             type="text"
             placeholder="dark class"
+            onChange={e => {
+              this.handleFilter(e, "darkClassText");
+            }}
+            value={darkClassText}
           />
           <input
             name="is-light"
             type="checkbox"
             className="is-light-checkbox"
+            onClick={e => {
+              this.handleIsLight(e);
+            }}
           />
           <label htmlFor="is-light">Is light</label>
-          <button type="submit" className="create-color">Create!</button>
+          <button type="submit" className="create-color">
+            Create!
+          </button>
         </form>
       </div>
     );
@@ -121,4 +194,3 @@ export default App;
 // * Por defecto el navegador envía una petición HTTP cuando se hace submit a un
 //   formulario, por esto es necesario utilizar el evento y
 //   el método preventDefault() para evitar que la página se recargue.
-
